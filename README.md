555ec485fc608ad80fed1dae882e0589b16b5484
harleysherman <harsatr11@icloud.com>
2023-12-21 23:30:34 -0600

    added README and worked through JavaScript character set logic

--- README.md
+++ README.md
@@ -1,1 +1,69 @@
 # passwordGenerator
+
+## Description
+
+Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+
+- The motivation of this project is to learn how to create java script logic.
+- This project looks to create a password generator.
+- The goal of the project is to use JavaScript to create a password generator.
+- I learned how to use singe if statements and for loops, and two different ways to logic: psuedo code and duck coding.
+
+## Table of Contents (Optional)
+
+If your README is long, add a table of contents to make it easy for users to find what they need.
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+Run this project through Google Chrome using the IO link provided.
+
+## Usage
+
+Provide instructions and examples for use. Include screenshots as needed.
+
+To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+
+    ```md
+    ![alt text](assets/images/screenshot.png)
+    ```
+
+## Credits
+
+I would like to thank Leif for helping start the logic of this project through psuedo code. I want to thank Charles, my tutor, for helping me learn to debug through console.log to figure out what the for loops and if statements are doing exactly.
+
+## License
+
+The license for this project is MIT license.
+
+---
+
+🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+
+## Badges
+
+There are no badges at this time.
+
+## Features
+
+Here are some features of the project:
+1. If the user presses cancel on first prompt, it will tell them thank you and end program.
+2. There is a range the user can enter. If the numer enetered in incorrect, the window will prompt the user for the correct one.
+3. The 'random' password generated includes at least one instance of all four attributes. 
+
+## How to Contribute
+
+There are no contributions at this time as this project is everywhere on the internet.
+[Contributor Covenant](https://www.contributor-covenant.org/) 
+
+## Tests
+
+Here are the edge cases that can be tested:
+1. For the number of characters the user would like in their password, please enter a number < 8 && number > 129
+2. Press cancel on the first prompt.
+3. Press cancel on all four attributes for the password.
+4. Press okay for only one attribute for the password.
--- script.js
+++ script.js
@@ -2,45 +2,107 @@
 var generateBtn = document.querySelector("#generate");
 
 function generatePassword () {
-  // In an array
-  var lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']; 
-  var uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
-  var numeric = [0,1,2,3,4,5,6,7,8,9];
-  var special = [`'`, '!', '"', '#', '$', '%', '&','(', ')', '*','+','-','.', `/`,':',';','<','=','>','?', '@',`[`,','[','`\`,`]`,'^','_',```,`{`,`|`,`}`,`~`];
-  var userPasswordLength; //aka password length
+    // In an array
+    var lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']; 
+    var uppercase = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+    var numeric = [0,1,2,3,4,5,6,7,8,9];
+    var special = [`'`, '!', '"', '#', '$', '%', '&','(', ')', '*','+','-','.', `/`,':',';','<','=','>','?', '@',`[`,`{`,`}`,`~`];
+    var userPasswordLength; //aka password length
+    var randomSpecial;
+    var randomLowercase;
+    var randomUppercase;
+    var randomNumeric;
+    var randomSpecial;
+    var userPassword = [];
 
-// GIVEN I need a new, secure password
-// WHEN I click the button to generate a password
-//Ask the user if they would like to generate a new password
-prompt ("Do you want to generate a new password?")
-//if user presses okay, continue with below
-// THEN I am presented with a series of prompts for password criteria
-//Ask the user a prompt for the password length
-  //Check user input to make sure bigger than or equal to eight 
-  //and less than 129
-  userPasswordLength = prompt ("How long would you link your password to be?");
-  if (userPasswordLength >= 8 || userPasswordLength <= 129) [
-    // WHEN prompted for password criteria
-    // THEN I select which criteria to include in the password
-    prompt("Do you want to include lowercase in your password?")
-    //if user presses okay randomize lowercase array
-    var randomLowercase = Math.floor(Math.random()*lowercase.length);
-    prompt("Include upppercase in your password?")
-    //if user presses okay randomize uppercase array
-    var randomUppercase = Math.floor(Math.random()*uppercase.length);
-    prompt("Will you include numbers in your password?")
-    //if user presses okay randomize numeric array
-    var randomNumeric = Math.floor(Math.random()*numeric.length);
-    prompt("Want to include special characters in your password?")
-    //if user presses okay randomize special array
-    var randomSpecial = Math.floor(Math.random()*special.length);
-    //else ignore
-  ] else {
-    userPasswordLength = prompt ("This is not the proper password length. Please enter a number between 8 and 128.");
-  }
-
-//else { prompt("Thanks for your input")}
-
+    // GIVEN I need a new, secure password
+    // WHEN I click the button to generate a password
+    //Ask the user if they would like to generate a new password
+    var createPassword =  confirm("Do you want to generate a new password?");
+    if (createPassword === true) {
+    //if user presses okay, continue with below
+    //Ask the user a prompt for the password length
+    userPasswordLength = parseInt(prompt("How long would you link your password to be?"));
+    console.log(userPasswordLength);
+    //Check user input to make sure bigger than or equal to eight 
+    //and less than 129
+    if (userPasswordLength >= 8 && userPasswordLength <= 128) {
+      // THEN I am presented with a series of prompts for password criteria
+      // WHEN prompted for password criteria
+      // THEN I select which criteria to include in the password
+      //for (int i = 0; i < userPassword; i++)
+      var addLowercase = confirm("Do you want to include lowercase in your password?");
+      //if user presses okay randomize lowercase array
+      if(addLowercase === true) {
+        randomLowercase = Math.floor(Math.random()*lowercase.length);
+        for (var i = 0; i < randomLowercase; i++) {
+          //Combine to userPassword []
+          userPassword = userPassword.concat(lowercase);
+          //decrease password length by one
+          //userPasswordLength--; 
+        }
+      } 
+      var addUppercase = confirm("Include upppercase in your password?");
+      if(addUppercase === true) { 
+        randomUppercase = Math.floor(Math.random()*uppercase.length);
+        for (var i = 0; i < randomUppercase; i++) {
+          //Combine to userPassword []
+          userPassword = userPassword.concat(uppercase);
+          //decrease password length by one
+          //userPasswordLength--; 
+        }
+      }
+      var addNumeric = confirm("Will you include numbers in your password?");
+      if(addNumeric === true) { 
+        //if user presses okay randomize numeric array
+        randomNumeric = Math.floor(Math.random()*numeric.length);
+        for (var i = 0; i < randomNumeric; i++) {
+          //Combine to userPassword []
+          userPassword = userPassword.concat(numeric);
+          //decrease password length by one
+          //userPasswordLength--; 
+        }
+      } 
+      //if user presses okay randomize special array
+      //   var randomSpecial = Math.floor(Math.random()*special.length);
+      var addSpecial = confirm("Want to include special characters in your password?");
+      //if user presses okay randomize special array
+      if(addSpecial === true) { 
+        //if user presses okay randomize numeric array
+        randomSpecial = Math.floor(Math.random()*special.length);     
+        for (var i = 0; i < randomSpecial; i++) {
+          //Combine to userPassword []
+          userPassword = userPassword.concat(special);
+          //decrease password length by one
+          //userPasswordLength--; 
+        }
+      } 
+      if(addLowercase === false && addUppercase === false && addNumeric === false && addSpecial === false) {
+        alert("Please selet one character set.");       
+        generatePassword();
+      }
+      
+      var randomPassword = [];
+      for (var i = 0; i < userPasswordLength; i++) {
+        var randomUserPassword = randomPassword.push(userPassword[Math.floor(Math.random()*userPassword.length)]); //START HERE, THEN GO BACKWARD, 85 to 93
+        userPasswordLength--; 
+        //userPassword = randomPassword.join();
+        // randomLowercase;
+        // randomUppercase;
+        // randomNumeric;
+        // randomSpecial;
+        //console.log(randomUserPassword);
+      }
+      console.log(randomPassword);
+      return randomPassword.join("");
+    } else {  //else ignore
+      alert("This is not the proper password length. Please enter a number between 8 and 128.");
+      generatePassword();
+    } 
+    } else {
+    confirm("Thanks for your input.");
+    }
+}
 
 // WHEN prompted for the length of the password
 // THEN I choose a length of at least 8 characters and no more than 128 characters
@@ -59,16 +121,12 @@ prompt ("Do you want to generate a new password?")
 
 // WHEN the password is generated
 // THEN the password is either displayed in an alert or written to the page
-  return "Hello World";
-}
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
-
 }
 
 // Add event listener to generate button
